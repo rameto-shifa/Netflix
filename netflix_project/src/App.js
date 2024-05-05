0d8e5aa8141966030ed839cbe3d3fb456834be33
rahmet <155022635+rameto-shifa@users.noreply.github.com>
2024-05-05 08:34:50 -1200

    header and footer

--- netflix_project/src/App.js
+++ netflix_project/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <h3>hi netflix project</h3>
+      <Home/>
     </div>
   );
 }
